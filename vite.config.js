c45b11dafa9f3e456f4c4d08a369f73645e7ddda
V0LKINI <k.volkov.n@gmail.com>
2024-01-20 19:33:36 +0700

    Docker

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
+const env = loadEnv('','./')
+const host = env.VITE_APP_URL;
+
 export default defineConfig({
+    build: {
+        target: 'esnext'
+    },
     plugins: [
-        vue(),
+        vue({
+            script: {
+                defineModel: true,
+                propsDestructure: true
+            }
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        host,
+        hmr: { host },
+    },
 });
