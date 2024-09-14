28ff983ba8adb329401ee83f05ed986cb1ccebac
V0LKINI <k.volkov.n@gmail.com>
2024-09-14 14:41:12 +0700

    Tabulation

--- resources/js/pages/Dictionary.vue
+++ resources/js/pages/Dictionary.vue
@@ -1,143 +1,163 @@
 <template>
-    <div class="dictionary">
-        <div class="head-wrapper">
-            <div class="filter">
-              <input-dropdown v-model="period" :options="periodOptions"/>
-              <input-search v-model.lazy="searchInput" label="Search" placeholder="Search for words"/>
-              <button-default @click.prevent="openWordDialog" text="Add new translation"/>
-            </div>
-            <div class="profile">
-              <div @click="toggleProfile()" v-click-outside="() => toggleProfile(true)" class="profile-info">
-                <img type="button" data-dropdown-placement="bottom-start" class="profile-info__image"
-                     :src="user.image != null ? user.image : defaultProfileImage" alt="User avatar">
-                <div>
-                  <div class="profile-info__name">{{ user.name }}</div>
-                  <div class="profile-info__joined">Joined in {{ user.created_at }}</div>
-                </div>
-              </div>
-              <!-- User dropdown -->
-              <div v-if="showMenu" class="profile-dropdown">
-                <div class="profile-dropdown__email">
-                  <div class="profile-dropdown__email-text">{{ user.email }}</div>
-                </div>
-                <div class="profile-dropdown__settings">
-                  <button @click="openProfileDialog()" class="profile-dropdown__settings-button">
-                    Settings
-                  </button>
-                </div>
-                <div class="profile-dropdown__signout">
-                  <button @click="authStore.logout()" class="profile-dropdown__signout-button">
-                    Sign out
-                  </button>
-                </div>
-              </div>
-            </div>
-        </div>
-        <div v-if="loadData" class="spinner-wrapper">
-          <spinner/>
-        </div>
-        <div v-else-if="!loadData && words.length === 0" class="not-found-wrapper">
-          <div class="not-found">
-            <div class="not-found-header">
-              <h2 class="h2">
-                <p>Sorry, we didn't find any words =( </p>
-                <p>To add new words, you need to follow a few steps:</p>
-              </h2>
-            </div>
-            <div class="not-found-stage">
-              <div class="not-found-stage__text">
-                <h3 class="h3">1: Press "Add new translation" button at the top of the screen</h3>
-              </div>
-            </div>
-            <div class="not-found-stage">
-              <div class="not-found-stage__text">
-                <h3 class="h3">2: Enter a word and its translation or use an auto translation icon</h3>
-              </div>
-            </div>
-            <div class="not-found-stage">
-              <div class="not-found-stage__text">
-                <h3 class="h3">3: Click "Save" button</h3>
-              </div>
-            </div>
+  <div class="dictionary">
+    <div class="head-wrapper">
+      <div class="filter">
+        <input-dropdown v-model="period" :options="periodOptions"/>
+        <input-search v-model.lazy="searchInput" label="Search" placeholder="Search for words"/>
+        <button-default @click.prevent="openWordDialog" text="Add new translation"/>
+      </div>
+      <div class="profile">
+        <div @click="toggleProfile()" v-click-outside="() => toggleProfile(true)" class="profile-info">
+          <img type="button" data-dropdown-placement="bottom-start" class="profile-info__image"
+               :src="user.image != null ? user.image : defaultProfileImage" alt="User avatar">
+          <div>
+            <div class="profile-info__name">{{ user.name }}</div>
+            <div class="profile-info__joined">Joined in {{ user.created_at }}</div>
           </div>
         </div>
-        <div v-else class="table-wrapper">
-          <table class="table">
-            <thead class="table-head">
-            <tr class="table-head__row">
-              <th @click="changeSort('word')" class="table-head__column">
-                <div class="table-head__column-content">
-                  Word
-                  <svg v-if="sortBy.word === 'ASC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19V5m0 14-4-4m4 4 4-4"/>
-                  </svg>
-                  <svg v-else-if="sortBy.word === 'DESC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
-                  </svg>
-                </div>
-              </th>
-              <th @click="changeSort('transcription')" class="table-head__column">
-                <div class="table-head__column-content">
-                  Transcription
-                  <svg v-if="sortBy.transcription === 'ASC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19V5m0 14-4-4m4 4 4-4"/>
-                  </svg>
-                  <svg v-else-if="sortBy.transcription === 'DESC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
-                  </svg>
-                </div>
-              </th>
-              <th @click="changeSort('translation')" class="table-head__column">
-                <div class="table-head__column-content">
-                  Translation
-                  <svg v-if="sortBy.translation === 'ASC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19V5m0 14-4-4m4 4 4-4"/>
-                  </svg>
-                  <svg v-else-if="sortBy.translation === 'DESC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
-                  </svg>
-                </div>
-              </th>
-              <th @click="changeSort('date')" class="table-head__column">
-                <div class="table-head__column-content">
-                  Date
-                  <svg v-if="sortBy.date === 'DESC'"  class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19V5m0 14-4-4m4 4 4-4"/>
-                  </svg>
-                  <svg v-else-if="sortBy.date === 'ASC'" class="table-head__column-arrow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
-                  </svg>
-                </div>
-              </th>
-              <th class="table-head__column">
-                Actions
-              </th>
-            </tr>
-            </thead>
-            <tbody class="table-body">
-            <tr v-for="word in words" class="table-body__row">
-              <th class="table-body__column">{{ word.text }}</th>
-              <td class="table-body__column">{{ word.transcription ?? '—' }}</td>
-              <td class="table-body__column">{{ word.translations }}</td>
-              <td class="table-body__column">{{ word.created_at }}</td>
-              <td class="table-body__column">
-                  <div @click.prevent="showWord(word.id)" class="table-body__column-icon">
-                    <svg class="table-body__column-icon-show" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
-                    </svg>
-                  </div>
-                  <div @click.prevent="deleteWord(word.id)" class="table-body__column-icon">
-                    <svg class="table-body__column-icon-delete" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
-                    </svg>
-                </div>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-          <pagination :pagination="pagination" :on-paginate="entityPaginate"/>
+        <!-- User dropdown -->
+        <div v-if="showMenu" class="profile-dropdown">
+          <div class="profile-dropdown__email">
+            <div class="profile-dropdown__email-text">{{ user.email }}</div>
+          </div>
+          <div class="profile-dropdown__settings">
+            <button @click="openProfileDialog()" class="profile-dropdown__settings-button">
+              Settings
+            </button>
+          </div>
+          <div class="profile-dropdown__signout">
+            <button @click="authStore.logout()" class="profile-dropdown__signout-button">
+              Sign out
+            </button>
+          </div>
         </div>
+      </div>
     </div>
+    <div v-if="loadData" class="spinner-wrapper">
+      <spinner/>
+    </div>
+    <div v-else-if="!loadData && words.length === 0" class="not-found-wrapper">
+      <div class="not-found">
+        <div class="not-found-header">
+          <h2 class="h2">
+            <p>Sorry, we didn't find any words =( </p>
+            <p>To add new words, you need to follow a few steps:</p>
+          </h2>
+        </div>
+        <div class="not-found-stage">
+          <div class="not-found-stage__text">
+            <h3 class="h3">1: Press "Add new translation" button at the top of the screen</h3>
+          </div>
+        </div>
+        <div class="not-found-stage">
+          <div class="not-found-stage__text">
+            <h3 class="h3">2: Enter a word and its translation or use an auto translation icon</h3>
+          </div>
+        </div>
+        <div class="not-found-stage">
+          <div class="not-found-stage__text">
+            <h3 class="h3">3: Click "Save" button</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="table-wrapper">
+      <table class="table">
+        <thead class="table-head">
+        <tr class="table-head__row">
+          <th @click="changeSort('word')" class="table-head__column">
+            <div class="table-head__column-content">
+              Word
+              <svg v-if="sortBy.word === 'ASC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 19V5m0 14-4-4m4 4 4-4"/>
+              </svg>
+              <svg v-else-if="sortBy.word === 'DESC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+              </svg>
+            </div>
+          </th>
+          <th @click="changeSort('transcription')" class="table-head__column">
+            <div class="table-head__column-content">
+              Transcription
+              <svg v-if="sortBy.transcription === 'ASC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 19V5m0 14-4-4m4 4 4-4"/>
+              </svg>
+              <svg v-else-if="sortBy.transcription === 'DESC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+              </svg>
+            </div>
+          </th>
+          <th @click="changeSort('translation')" class="table-head__column">
+            <div class="table-head__column-content">
+              Translation
+              <svg v-if="sortBy.translation === 'ASC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 19V5m0 14-4-4m4 4 4-4"/>
+              </svg>
+              <svg v-else-if="sortBy.translation === 'DESC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+              </svg>
+            </div>
+          </th>
+          <th @click="changeSort('date')" class="table-head__column">
+            <div class="table-head__column-content">
+              Date
+              <svg v-if="sortBy.date === 'DESC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 19V5m0 14-4-4m4 4 4-4"/>
+              </svg>
+              <svg v-else-if="sortBy.date === 'ASC'" class="table-head__column-arrow" aria-hidden="true"
+                   xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 6v13m0-13 4 4m-4-4-4 4"/>
+              </svg>
+            </div>
+          </th>
+          <th class="table-head__column">
+            Actions
+          </th>
+        </tr>
+        </thead>
+        <tbody class="table-body">
+        <tr v-for="word in words" class="table-body__row">
+          <th class="table-body__column">{{ word.text }}</th>
+          <td class="table-body__column">{{ word.transcription ?? '—' }}</td>
+          <td class="table-body__column">{{ word.translations }}</td>
+          <td class="table-body__column">{{ word.created_at }}</td>
+          <td class="table-body__column">
+            <div @click.prevent="showWord(word.id)" class="table-body__column-icon">
+              <svg class="table-body__column-icon-show" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                   height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"/>
+              </svg>
+            </div>
+            <div @click.prevent="deleteWord(word.id)" class="table-body__column-icon">
+              <svg class="table-body__column-icon-delete" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                   width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M6 18 17.94 6M18 18 6.06 6"/>
+              </svg>
+            </div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <pagination :pagination="pagination" :on-paginate="entityPaginate"/>
+    </div>
+  </div>
 
   <modal v-if="showProfileDialog" @closeDialog="closeProfileDialog">
     <template #title>Edit Profile</template>
@@ -150,8 +170,10 @@
               <div class="profile-form__dropdown-image">
                 <img :src="image" class="profile-form__dropdown-image-img" alt="profile-image">
                 <button @click.prevent="deleteImage" class="profile-form__dropdown-image-button">
-                  <svg class="profile-form__dropdown-image-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                  <svg class="profile-form__dropdown-image-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                   </svg>
                 </button>
               </div>
