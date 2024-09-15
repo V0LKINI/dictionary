2bae220a07d568c575d113346604eef8d2405cf4
V0LKINI <k.volkov.n@gmail.com>
2024-09-15 19:49:17 +0700

    Fix 401 problem

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,20 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL_API;
 
+axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response.status === 401) {
+            localStorage.removeItem('authUser');
+            localStorage.removeItem('token');
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 if (localStorage.getItem('token')) {
     setHeaderAuth(localStorage.getItem('token'))
 } else {
