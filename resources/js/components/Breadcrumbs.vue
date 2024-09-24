90c9f3962a163612fef2f1dcf19c0ea56a0d5937
V0LKINI <k.volkov.n@gmail.com>
2024-09-24 23:23:16 +0700

    Breadcrumbs back button & Fixes

---
+++ resources/js/components/Breadcrumbs.vue
@@ -0,0 +1,10 @@
+<template>
+ <router-link :to="{name: 'Dictionary'}" class="breadcrumbs">
+  <svg class="breadcrumbs__icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14 8-4 4 4 4"/>
+  </svg>
+  <button class="breadcrumbs__text">
+   Back
+  </button>
+ </router-link >
+</template>
--- resources/js/pages/Exercises/FindTranslation.vue
+++ resources/js/pages/Exercises/FindTranslation.vue
@@ -1,20 +1,24 @@
 <template>
  <div class="findTranslation">
-  <navigation/>
-  <div class="card">
-    <div class="card__top">
-      Cat
-    </div>
-    <div class="card__body">
-      <div class="card__answer">Собака</div>
-      <div class="card__answer">Дом</div>
-      <div class="card__answer">Стол</div>
-      <div class="card__answer">Стул</div>
-    </div>
+  <div class="head">
+   <breadcrumbs/>
+   <navigation/>
   </div>
+<!--  <div class="card">-->
+<!--    <div class="card__top">-->
+<!--      Cat-->
+<!--    </div>-->
+<!--    <div class="card__body">-->
+<!--      <div class="card__answer">Собака</div>-->
+<!--      <div class="card__answer">Дом</div>-->
+<!--      <div class="card__answer">Стол</div>-->
+<!--      <div class="card__answer">Стул</div>-->
+<!--    </div>-->
+<!--  </div>-->
  </div>
 </template>
 
 <script setup>
   import Navigation from "../../layouts/Navigation.vue";
+  import Breadcrumbs from "../../components/Breadcrumbs.vue";
 </script>
