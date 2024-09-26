271fafdb261596061a0a0730205352cb1a699351
V0LKINI <k.volkov.n@gmail.com>
2024-09-26 23:35:34 +0700

    Find translation exercise (1)

--- resources/js/pages/Exercises/FindTranslation.vue
+++ resources/js/pages/Exercises/FindTranslation.vue
@@ -1,24 +1,65 @@
 <template>
  <div class="findTranslation">
-  <div class="head">
+  <div class="head-wrapper">
    <breadcrumbs/>
    <navigation/>
   </div>
-<!--  <div class="card">-->
-<!--    <div class="card__top">-->
-<!--      Cat-->
-<!--    </div>-->
-<!--    <div class="card__body">-->
-<!--      <div class="card__answer">Собака</div>-->
-<!--      <div class="card__answer">Дом</div>-->
-<!--      <div class="card__answer">Стол</div>-->
-<!--      <div class="card__answer">Стул</div>-->
-<!--    </div>-->
-<!--  </div>-->
+  <div v-if="loadData" class="spinner-wrapper">
+   <spinner/>
+  </div>
+  <div v-else class="exercise-wrapper">
+   <div class="card">
+    <div class="card__header">
+     {{ sample.text }}
+    </div>
+    <div class="card__body">
+     <div v-for="word in words"
+          @click="checkAnswer(word)"
+          class="card__answer"
+          :class="{'card__answer-correct': word.result === true, 'card__answer-error': word.result === false}"
+     >
+       {{ word.translations }}
+     </div>
+    </div>
+   </div>
+  </div>
  </div>
 </template>
 
 <script setup>
   import Navigation from "../../layouts/Navigation.vue";
   import Breadcrumbs from "../../components/Breadcrumbs.vue";
+  import axios from "axios";
+  import {ref} from "vue";
+  import Spinner from "../../components/Spinner.vue";
+
+  const loadData = ref(false)
+  const sample = ref(null)
+  const words = ref([])
+
+  const getSample = () => {
+      loadData.value = true
+
+      axios.get('/api/exercises/findTranslation/getData').then((r) => {
+          loadData.value = false
+
+          let response = r.data;
+
+          if (response.success) {
+            sample.value = response.data.sample
+            words.value = response.data.words
+          }
+      });
+  }
+
+  const checkAnswer = (answer) => {
+    let index = words.value.findIndex(element => element.id === answer.id);
+    let result = answer.id === sample.value.id;
+
+    words.value[index].result = result
+  }
+
+  //initial sample
+  getSample()
+
 </script>
